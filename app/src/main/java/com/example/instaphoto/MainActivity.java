bf774ff347c9ef32e5064602d0cb5fa6a88ec1ab
walter broncano <santiago25392@gmail.com>
2020-10-31 03:37:36 -0400

    last

--- app/src/main/java/com/example/instaphoto/MainActivity.java
+++ app/src/main/java/com/example/instaphoto/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.instaphoto;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -13,14 +16,20 @@ import android.os.Environment;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.instaphoto.fragments.ComposeFragment;
+import com.example.instaphoto.fragments.PostsFragment;
+import com.example.instaphoto.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
@@ -30,14 +39,10 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+
     public static final String TAG = "MainActivity";
-    private EditText etDescription;
-    private Button btnCaptureImage;
-    private ImageView ivPostImage;
-    private Button btnSubmit;
-    private File photoFile;
-    public String photoFileName = "photo.jpg";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
 
     @Override
@@ -45,34 +50,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etDescription = findViewById(R.id.etDescription);
-        btnCaptureImage = findViewById(R.id.btnCapture);
-        ivPostImage = findViewById(R.id.PostImage);
-        btnSubmit = findViewById(R.id.btnSubmit);
 
-        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                launchCamera();
-            }
-        });
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String description = etDescription.getText().toString();
+                Fragment fragment;
+                switch (menuItem.getItemId()){
+                    case R.id.action_home:
+                        //TODO
+                      //  Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        fragment = new PostsFragment();
+                        break;
+                    case R.id.action_compose:
+                      //  Toast.makeText(MainActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                     //   Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                        fragment = new ProfileFragment();
+                        break;
 
-                if(description.isEmpty()){
-                    Toast.makeText(MainActivity.this, "Dexcription cannot be empty", Toast.LENGTH_SHORT).show();
-                    return;
                 }
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                savePost(description, currentUser);
+
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
         });
+
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
 
 
     }
+
+    /**
 
     private void launchCamera() {
 
@@ -130,10 +145,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void savePost(String description, ParseUser currentUser) {
+    private void savePost(String description, ParseUser currentUser, File photoFile) {
         Post post = new Post();
 
         post.setDescription(description);
+        post.setImage(new ParseFile(photoFile));
         post.setUser(currentUser);
         post.saveInBackground(new SaveCallback() {
             @Override
@@ -144,6 +160,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 Log.i(TAG, "Post save was successful!!");
                 etDescription.setText("");
+                ivPostImage.setImageResource(0);
             }
         });
     }
@@ -164,5 +181,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    */
 
 }
